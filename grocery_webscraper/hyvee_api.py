0270f4f18524635f80d0847fac7fe8fd32d8820f
= <aaronjencks@aol.com>
2020-01-17 11:00:49 -0600

    Worked on getting hyvee pages to work

--- grocery_webscraper/hyvee_api.py
+++ grocery_webscraper/hyvee_api.py
@@ -77,11 +77,17 @@ def find_recent_purchases(browser: webdriver.Firefox, logged_in: bool, timeout_d
         print('Logging in')
         login(browser, timeout_delay=timeout_delay)
 
-    # Navigates to the previously bought items lists
-    browser.get(hyvee_home + 'grocery/my-account/lists/frequent-purchases.aspx')
+    while True:
+        try:
+            # Navigates to the previously bought items lists
+            browser.get(hyvee_home + 'grocery/my-account/lists/frequent-purchases.aspx')
 
-    print('Loading page')
-    total_count = WebDriverWait(browser, timeout_delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'span[id=ctl00_ContentPlaceHolder1_totalItems]')))
+            print('Loading page')
+            total_count = WebDriverWait(browser, timeout_delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'span[id=ctl00_ContentPlaceHolder1_totalItems]')))
+            break
+        except TimeoutException as _:
+            print('Failed to load page')
+            browser.refresh()
 
     while True:
         try:
